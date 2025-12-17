a15a8b0d4fade526f065c93ac44d3df346a26ac6
murphy-afk <bardarenoemi@gmail.com>
2025-12-17 15:23:27 +0100

    feat: posts route

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express'; 
+import postsRouter from "./routers/posts.js";
 
 const app = express();
 const port = 3006;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.send('server works')
+})
+
+app.use('/posts', postsRouter)
 
 app.listen(port, function () {
   console.log("Server is listening on port: " + port);
--- routers/posts.js
+++ routers/posts.js
@@ -1,5 +1,9 @@
 import express from 'express';
 
-const router = express.Router;
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('posts route')
+})
 
 export default router
