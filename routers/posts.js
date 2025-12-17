c73f2e64b96b32f660a5a88d46c7114dbc3dee53
murphy-afk <bardarenoemi@gmail.com>
2025-12-17 15:37:10 +0100

    feat: show, create, update, modify, delete routes

--- routers/posts.js
+++ routers/posts.js
@@ -1,9 +1,44 @@
 import express from 'express';
+import { posts } from '../data.js'
 
 const router = express.Router();
 
+// INDEX
 router.get('/', (req, res) => {
-  res.send('posts route')
+  const result = {
+    info: {
+      count: posts.length,
+    },
+    result: posts,
+  }
+  res.json(result)
 })
+
+// SINGLE POST (SHOW)
+router.get('/:id', (req, res) => {
+  res.send('single post route')
+})
+
+// CREATE
+router.post('/', (req, res) => {
+  res.send('create new post route')
+})
+
+// UPDATE POST
+router.put('/:id', (req, res) => {
+  res.send('update existing post route')
+})
+
+// MODIFY 
+router.patch('/:id', (req, res) => {
+  res.send('modify existing post route')
+})
+
+// DELETE POST 
+router.delete('/:id', (req, res) => {
+  res.send('delete existing post')
+})
+
+
 
 export default router
